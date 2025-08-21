84acc142f80e8a98bf2d56e63a2963a33598c1a2
Arad David <arado3333@gmail.com>
2025-08-21 13:47:37 +0300

    Update server.js

--- page-by-page-api/server.js
+++ page-by-page-api/server.js
@@ -12,7 +12,10 @@ import goalsRouter from "./services/goals/goals.routes.js";
 const PORT = process.env.PORT || 5500;
 
 const server = express();
-server.use(cors());
+server.use(cors({
+    origin: "http://localhost:8081",
+    credentials: true,
+}));
 server.use(express.json()); 
 server.use(express.urlencoded({ extended: true })); //תמיכה בכתובת בתווים שאינם לטיניים
 
