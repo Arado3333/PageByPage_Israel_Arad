0cc2912cd7b3651312f40c353649d4aaab48a8ed
Arad David <arado3333@gmail.com>
2025-08-21 12:38:41 +0300

    Update ai.controller.js

--- page-by-page-api/services/ai_assistant/controllers/ai.controller.js
+++ page-by-page-api/services/ai_assistant/controllers/ai.controller.js
@@ -2,28 +2,28 @@
 
 import { GoogleGenAI } from "@google/genai";
 
-export async function textChat() {
-    const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+export async function textChat(req, res) {
+  const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 
-    try {
-        let chat = ai.chats.create({
-            model: req.body.model,
-            history: req.body.history,
-            config: {
-                temperature: 0.5,
-                maxOutputTokens: 1200,
-                systemInstruction: req.body.systemInstruction,
-            },
-        });
+  try {
+    let chat = ai.chats.create({
+      model: req.body.model,
+      history: req.body.history,
+      config: {
+        temperature: 0.5,
+        maxOutputTokens: 1200,
+        systemInstruction: req.body.systemInstruction,
+      },
+    });
 
-        let response = await chat.sendMessage({
-            message: req.body.message,
-        });
-        res.status(200).json({
-            role: "model",
-            parts: [{ text: response.text }],
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Error sending message to gemini" });
-    }
+    let response = await chat.sendMessage({
+      message: req.body.message,
+    });
+    res.status(200).json({
+      role: "model",
+      parts: [{ text: response.text }],
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error sending message to gemini" });
+  }
 }
